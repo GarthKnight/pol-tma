51f54c0734e9ac375a67ec3ff86213eeb5381c2e
stepan logvinov <st.logvinov@gmail.com>
2024-06-18 23:14:17 +0300

    fix

--- src/contracts/Getters.ts
+++ src/contracts/Getters.ts
@@ -6,46 +6,51 @@ import { Constants } from "./Constants";
 const contractAddress = Address.parse(Constants.addressString);
 
 export async function getIsFinished() {
-  // initialize ton rpc client on testnet
-  const endpoint = await getHttpEndpoint({ network: "testnet" });
-  const client = new TonClient({ endpoint });
+    // initialize ton rpc client on testnet
+    const endpoint = await getHttpEndpoint({ network: "testnet" });
+    const client = new TonClient({ endpoint });
 
-  // open Counter instance by address
-  // replace with your address from step 8
-  const counter = new MyContract(contractAddress);
-  const counterContract = client.open(counter);
+    // open Counter instance by address
+    // replace with your address from step 8
+    const counter = new MyContract(contractAddress);
+    const counterContract = client.open(counter);
 
-  // call the getter on chain
-  const counterValue = await counterContract.getIsFinalized();
-  console.log("value 2:", counterValue.toString());
+    // call the getter on chain
+    const counterValue = await counterContract.getIsFinalized();
+    console.log("value 2:", counterValue.toString());
 }
-  
+
 export async function getTotalBetA() {
     // initialize ton rpc client on testnet
     const endpoint = await getHttpEndpoint({ network: "testnet" });
     const client = new TonClient({ endpoint });
-  
+
     // open Counter instance by address
     // replace with your address from step 8
     const counter = new MyContract(contractAddress);
     const counterContract = client.open(counter);
-  
+
     // call the getter on chain
     const counterValue = await counterContract.getTotalBetA();
     console.log("value:", counterValue.toString());
-  }
+}
 
-  export async function getGetTrumpBidenData() {
+export async function getGetTrumpBidenData() {
     // initialize ton rpc client on testnet
     const endpoint = await getHttpEndpoint({ network: "testnet" });
     const client = new TonClient({ endpoint });
-  
+
     // open Counter instance by address
     // replace with your address from step 8
     const counter = new MyContract(contractAddress);
     const counterContract = client.open(counter);
-  
+
     // call the getter on chain
-    const counterValue = await counterContract.getGetTrumpBidenData();
-    console.log("value:", counterValue.toString());
-  }
+    const trump = await counterContract.getGetTrumpBidenData();
+
+    console.log("value:", trump.bet_a_name);
+    console.log("value:", trump.bet_b_name);
+    console.log("value:", trump.image);
+    console.log("value:", trump.odds_a);
+    console.log("value:", trump.odds_b);
+}
