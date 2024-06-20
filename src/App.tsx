8f234efd46679ab23565b6e409636a780bf777a6
stepan logvinov <st.logvinov@gmail.com>
2024-06-20 23:00:35 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 import { useState } from 'react';
 import DialogComponent from './components/DialogComponent';
 import BetInput from './components/BetInput';
@@ -18,54 +18,65 @@ function App() {
   };
 
   return (
-    <div className='app'>
-      <TonConnectButton />
 
-      <div className="button-container">
-        <button onClick={() => handleOpenDialog('1')}>Send to 1</button>
-        <button onClick={() => handleOpenDialog('2')}>Send to 2</button>
-      </div>
+    <TonConnectUIProvider
+      manifestUrl="https://garthknight.github.io/pol-tma/tonconnect-manifest.json"
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/Polygame_bot'
+      }}
+    >
+      {
+        <div className='app'>
+          <TonConnectButton />
 
-      <DialogComponent
-        open={openDialog}
-        onClose={handleCloseDialog}
-        title={"We are betting on: " + betType}
-        content={
-          <BetInput
-            onConfirm={handleCloseDialog}
+          <div className="button-container">
+            <button onClick={() => handleOpenDialog('1')}>Send to 1</button>
+            <button onClick={() => handleOpenDialog('2')}>Send to 2</button>
+          </div>
+
+          <DialogComponent
+            open={openDialog}
+            onClose={handleCloseDialog}
+            title={"We are betting on: " + betType}
+            content={
+              <BetInput
+                onConfirm={handleCloseDialog}
+                bet={betType}
+              />
+            }
             bet={betType}
           />
-        }
-        bet={betType}
-      />
 
-      {/* <GetBetAButton />
+          {/* <GetBetAButton />
+     
+           <SendBetOnAButton />
+     
+           <IsFinButton />
+     
+           <FinishButton />
+     
+           <button onClick={() =>
+             sendStringMessage("1")
+           }>
+             First option
+           </button>
+     
+           <button onClick={() =>
+             tonConnectUI.sendTransaction(createTransactionForStringMessage("1", "0.5"))
+           }>
+             First option
+           </button>
+     
+     
+           <button onClick={() =>
+             getGetTrumpBidenData()
+           }>
+             Get trump
+           </button> */}
+        </div >
+      }
+    </TonConnectUIProvider>
 
-      <SendBetOnAButton />
-
-      <IsFinButton />
-
-      <FinishButton />
-
-      <button onClick={() =>
-        sendStringMessage("1")
-      }>
-        First option
-      </button>
-
-      <button onClick={() =>
-        tonConnectUI.sendTransaction(createTransactionForStringMessage("1", "0.5"))
-      }>
-        First option
-      </button>
-
-
-      <button onClick={() =>
-        getGetTrumpBidenData()
-      }>
-        Get trump
-      </button> */}
-    </div >
   );
 }
 
