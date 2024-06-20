7d66db51d107e0a26fa5465ccb30f3e820788901
stepan logvinov <st.logvinov@gmail.com>
2024-06-20 21:18:12 +0300

    update gitignore

---
+++ src/contracts/Constants.ts
@@ -0,0 +1,5 @@
+export class Constants {
+    static addressString = "EQCifEQy8Ufru3-VLLqFeA3QdaUNqXAAnSlc96FuV6jwW0a7"
+    // static mnemonic = "conduct civil mouse vault ice differ blouse cheese smile card rail give conduct memory item moon resemble decline girl where announce hurry upper struggle";
+    static mnemonic = "sheriff skull merry snake normal gold surprise oval category case spatial pond version chase cake reduce surge faith finger dash plug hybrid weasel isolate";
+}
