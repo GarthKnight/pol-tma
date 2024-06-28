aaec42bd81903fcd6a23302bf8664e59f50e274e
stepan logvinov <st.logvinov@gmail.com>
2024-06-28 14:05:55 +0300

    eruda

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import eruda from 'eruda';
 
 const manifestUrl = 'https://garthknight.github.io/jsonstorage/tonconnect-manifest.json';
+eruda.init()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
